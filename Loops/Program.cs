0882da89ba814964becd0a3e459ea2f0e5e3472f
Kiss <l3alazskiss15@gmail.com>
2022-10-31 11:22:31 -0300

    Initial commit

---
+++ Loops/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int end = 10;
+            int[] ammo = { 6, 2, 25, 50, 10};
+
+            Console.WriteLine("While Loop:");
+            Console.WriteLine();
+
+            while (i <= end)
+            {
+                Console.Write(i + ", ");
+                i++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("For Loop:");
+            Console.WriteLine();
+
+            for (i = 1; i <= end; i++)
+                Console.Write(i + ", ");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Weapon Ammo:");
+            Console.WriteLine();
+
+            i = 0;
+            end = ammo.Length;
+
+            while(i < end)
+            {
+                if(i != ammo.Length - 1)
+                    Console.Write("Weapon #" + i + " ammo: " + ammo[i] + ", ");
+                else
+                    Console.Write("Weapon #" + i + " ammo: " + ammo[i]);
+                i++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
